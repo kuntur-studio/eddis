f945d83ecb84188d4402764cb74836c15bdca718
Kuntur Studio <kunturstudio@gmail.com>
2025-04-23 13:19:50 -0300

    Modifica la función localize_branches_data para obtener el título del branch utilizando get_the_title en lugar de wp_specialchars_decode.

--- wp-content/themes/eddis/custom-functions/assets.php
+++ wp-content/themes/eddis/custom-functions/assets.php
@@ -102,7 +102,7 @@ function localize_branches_data() {
 
                             $branch_data = [
                                 'id' => $branch_id,
-                                'title' => wp_specialchars_decode(get_post_field('post_title', $branch_id, 'raw')),
+                                'title' => get_the_title(),
                                 'eddis_system_id' => carbon_get_post_meta($branch_id, 'eddis_system_id'),
                                 'description' => carbon_get_post_meta($branch_id, 'description'),
                                 'address' => carbon_get_post_meta($branch_id, 'address'),
