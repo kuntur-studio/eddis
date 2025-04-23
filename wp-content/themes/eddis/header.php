e4b3bbf521637704dfc4de0198eb7c54dc2e48db
Kuntur Studio <kunturstudio@gmail.com>
2025-04-23 20:20:30 -0300

    Pruebas con el header

--- wp-content/themes/eddis/header.php
+++ wp-content/themes/eddis/header.php
@@ -6,10 +6,10 @@
     <title>Nuevo Sitio Web</title>
 	<?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 
     <header>
-        <h1>Este es el header</h1>
+        <h1>Este es el header cambiado</h1>
     </header>
 
     <main>
