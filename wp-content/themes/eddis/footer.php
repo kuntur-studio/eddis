9dd5f10899c25da07746637368d7cf6af0644c2c
Kuntur Studio <kunturstudio@gmail.com>
2025-04-24 20:13:35 -0300

    Refactor el pie de página para utilizar opciones de menú y redes sociales desde Carbon Fields, y mejora la lógica de contacto telefónico y correo electrónico.

--- wp-content/themes/eddis/footer.php
+++ wp-content/themes/eddis/footer.php
@@ -1,4 +1,4 @@
-    <footer class="page-footer">
+<footer class="page-footer">
         <div class="container prefooter">
             <div class="row">
                 <div class="col-md-12 col-xs-12">
@@ -11,54 +11,83 @@
                         <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 text-lg-left text-md-center text-sm-center text-center menufooter">
                             <h3>Menú</h3>
                             <ul class="menu ">
-                            <?php /*if( have_rows('menu_footer','option') ):?>
-                                <?php while ( have_rows('menu_footer','option') ) : the_row();?>
-                                    <li><a href="<?php the_sub_field('link','option');?>"><?php the_sub_field('titulo');?></a></li>
-                                <?php endwhile;?>
-                            <?php endif;*/?>
+                            <?php
+                            $footer_menu_items = carbon_get_theme_option('footer_menu_items');
+                            if (!empty($footer_menu_items)) :
+                                foreach ($footer_menu_items as $item) :
+                                    $link = '';
+                                    if ($item['is_internal']) {
+                                        $link = get_permalink($item['page']);
+                                    } else {
+                                        $link = esc_url($item['external_url']);
+                                    }
+                                    ?>
+                                    <li><a href="<?php echo $link; ?>"><?php echo esc_html($item['title']); ?></a></li>
+                                <?php endforeach;
+                            endif;
+                            ?>
                             </ul>
                         </div>
                         <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 text-lg-left text-md-center text-sm-center text-center">
                             <h3>Redes</h3>
-                            <?php /*if( have_rows('redes_sociales', 'option') ):?>
-                                <?php while ( have_rows('redes_sociales', 'option') ) : the_row();?>
-                                    <a class="social-icon transition-280"  target="_blank" href="<?php the_sub_field('link', 'option');?>" class="m-1"><?php the_sub_field('icon', 'option');?></a>
-                                <?php endwhile;?>
-                            <?php endif;*/?>
+                            <?php
+                            $social_networks = carbon_get_theme_option('social_networks');
+                            if (!empty($social_networks)) :
+                                foreach ($social_networks as $network) :
+                                    ?>
+                                    <a class="social-icon transition-280"  target="_blank" href="<?php echo esc_url($network['link']); ?>" class="m-1"><?php echo $network['icon']; ?></a>
+                                <?php endforeach;
+                            endif;
+                            ?>
                         </div>
                         <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 text-lg-left text-md-center text-sm-center text-center">
                             <h3>Contacto</h3>
-                            <h2><a class="tel-footer" href="tel:+<?php /*the_field('telefono', 'option');*/?>"><i class="fas fa-phone"></i> <?php /*the_field('telefono', 'option');*/?></a></h2>
+                            <h2>
+                                <?php
+                                $contact_phone_active = carbon_get_theme_option('contact_phone_active');
+                                $contact_phone = carbon_get_theme_option('contact_phone');
+                                if ($contact_phone_active && !empty($contact_phone)) :
+                                    ?>
+                                    <a class="tel-footer" href="tel:+<?php echo preg_replace('/[^0-9]/', '', $contact_phone); ?>"><i class="fas fa-phone"></i> <?php echo esc_html($contact_phone); ?></a>
+                                    <?php
+                                endif;
+                                ?>
+                            </h2>
                             <ul class="menu ">
-                            <?php /*if( have_rows('menu_footer_contacto','option') ):?>
-                                <?php while ( have_rows('menu_footer_contacto','option') ) : the_row();?>
-                                    <li><a href="<?php the_sub_field('link','option');?>"><?php the_sub_field('titulo');?></a></li>
-                                <?php endwhile;?>
-                            <?php endif;*/?>
+                                <?php
+                                $contact_email_active = carbon_get_theme_option('contact_email_active');
+                                $contact_email = carbon_get_theme_option('contact_email');
+                                if ($contact_email_active && !empty($contact_email)) :
+                                    ?>
+                                    <li><a href="mailto:<?php echo esc_html($contact_email); ?>"><?php echo esc_html($contact_email); ?></a></li>
+                                    <?php
+                                endif;
+                                ?>
+                                <li><a href="/contacto/">Contacto</a></li>
                             </ul>
                         </div>
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-lg-left text-md-center text-sm-center text-center mt-2 mb-2 sedecentralfooter">
                             <h3>Sede Central</h3>
-                            <p><?php /*the_field('sede_central', 'option');*/?></p>
+                            <p><?php /*echo carbon_get_theme_option('sede_central');*/ ?></p>
                         </div>
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-lg-left text-md-center text-sm-center text-center mt-2 mb-2">
                         <p class="woocommerce-mini-cart__buttons buttons boton-de-arrepentimiento"><a href="https://www.eddis.edu.ar/arrepentimiento-de-compra/" class="btn btn-primary d-inline-block">Botón de arrepentimiento</a></p>
                         </div>
                     </div>
-                </div>
-                <!-- Acá iba el formulario del footer -->
-            </div>
-        </div>
-    <div class="bar-copyright p-4 mt-5 copyright-footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12">
-                    <p class="text-lg-left text-sm-center text-center">© Copyright <?php echo date("Y");?> | <?php bloginfo('name');?> | Todos los derechos reservados</p>	
+                    <!-- Acá iba el formulario del footer -->
                 </div>
             </div>
         </div>
-    </div>
+        <div class="bar-copyright p-4 mt-5 copyright-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <p class="text-lg-left text-sm-center text-center">© Copyright <?php echo date("Y");?> | <?php bloginfo('name');?> | Todos los derechos reservados</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </footer>
-	<?php wp_footer(); ?>
+    <?php wp_footer(); ?>
 </body>
 </html>
