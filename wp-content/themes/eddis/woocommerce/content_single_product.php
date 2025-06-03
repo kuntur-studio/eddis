7b11ce741deef7edd124ab8bbedd65c881130de0
Kuntur Studio <kunturstudio@gmail.com>
2025-06-03 13:15:50 -0300

    Inicio personalización de la página de producto.

--- wp-content/themes/eddis/woocommerce/content_single_product.php
+++ wp-content/themes/eddis/woocommerce/content_single_product.php
@@ -1,76 +1,69 @@
 <?php
 /**
- * The template for displaying product content in the single-product.php template
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/content-single-product.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see     https://woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 3.6.0
+ * Plantilla para mostrar el contenido del producto en la plantilla single-product.php
  */
 
 defined( 'ABSPATH' ) || exit;
 
 global $product;
 
-/**
- * Hook: woocommerce_before_single_product.
- *
- * @hooked woocommerce_output_all_notices - 10
- */
-do_action( 'woocommerce_before_single_product' );
+do_action( 'woocommerce_before_single_product' ); ?>
 
-if ( post_password_required() ) {
-	echo get_the_password_form(); // WPCS: XSS ok.
-	return;
-}
-?>
-<div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
+<div id="product-<?php the_ID(); ?>" <?php wc_product_class( 'course-single-wrapper', $product ); ?>>
 
-	<?php
-	/**
-	 * Hook: woocommerce_before_single_product_summary.
-	 *
-	 * @hooked woocommerce_show_product_sale_flash - 10
-	 * @hooked woocommerce_show_product_images - 20
-	 */
-	do_action( 'woocommerce_before_single_product_summary' );
-	?>
+    <div class="course-image">
+        <?php
+        /**
+         * Muestra la galería o imagen destacada del producto.
+         */
+        do_action( 'woocommerce_before_single_product_summary' );
+        ?>
+    </div>
 
-	<div class="summary entry-summary">
-		<?php
-		/**
-		 * Hook: woocommerce_single_product_summary.
-		 *
-		 * @hooked woocommerce_template_single_title - 5
-		 * @hooked woocommerce_template_single_rating - 10
-		 * @hooked woocommerce_template_single_price - 10
-		 * @hooked woocommerce_template_single_excerpt - 20
-		 * @hooked woocommerce_template_single_add_to_cart - 30
-		 * @hooked woocommerce_template_single_meta - 40
-		 * @hooked woocommerce_template_single_sharing - 50
-		 * @hooked WC_Structured_Data::generate_product_data() - 60
-		 */
-		do_action( 'woocommerce_single_product_summary' );
-		?>
-	</div>
+    <div class="course-detalle">
+        <?php
+        /**
+         * Elimino los hooks por defecto para personalizar el contenido más adelante.
+         */
+        remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+        remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+        remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+        remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+        remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+        remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50 );
 
-	<?php
-	/**
-	 * Hook: woocommerce_after_single_product_summary.
-	 *
-	 * @hooked woocommerce_output_product_data_tabs - 10
-	 * @hooked woocommerce_upsell_display - 15
-	 * @hooked woocommerce_output_related_products - 20
-	 */
-	do_action( 'woocommerce_after_single_product_summary' );
-	?>
+        /**
+         * Agrego contenido personalizado.
+         */
+        add_action( 'woocommerce_single_product_summary', function () use ( $product ) {
+            echo '<div class="course-contenido">';
+
+            // Título del curso
+            echo '<h1 class="course-title">' . get_the_title() . '</h1>';
+
+            // Descripción corta
+            $short_description = apply_filters( 'woocommerce_short_description', $product->get_short_description() );
+            if ( $short_description ) {
+                echo '<div class="course-description">' . $short_description . '</div>';
+            }
+
+            // Precio
+            echo '<div class="course-price">' . $product->get_price_html() . '</div>';
+
+            // Botón de inscripción (add to cart)
+            woocommerce_template_single_add_to_cart();
+
+            echo '</div>';
+        }, 5 );
+
+        /**
+         * Ejecuto la acción con el contenido del producto.
+         */
+        do_action( 'woocommerce_single_product_summary' );
+        ?>
+    </div>
 </div>
+
+<?php do_action( 'woocommerce_after_single_product_summary' ); ?>
 
 <?php do_action( 'woocommerce_after_single_product' ); ?>
