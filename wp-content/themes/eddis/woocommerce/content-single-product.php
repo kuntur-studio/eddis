c4f42bd964ac0259f752283c89958d746e8a0288
Kuntur Studio <kunturstudio@gmail.com>
2025-06-05 12:05:31 -0300

    prueba

--- wp-content/themes/eddis/woocommerce/content-single-product.php
+++ wp-content/themes/eddis/woocommerce/content-single-product.php
@@ -55,7 +55,7 @@ do_action( 'woocommerce_before_single_product' ); ?>
                         // Especificaciones del curso ?>
                         <div class="course-specifications">
                             <ul>
-                                <li><i class="fas fa-clock"></i> Duración: <?php echo $product_id, ' ', carbon_get_post_meta(763, 'duration'); ?></li>
+                                <li><i class="fas fa-clock"></i> Duración: <?php echo 'ID Producto', $product_id, ' ', carbon_get_post_meta(763, 'duration'); ?></li>
                                 <li><i class="fas fa-graduation-cap"></i> Certificación: <?php echo carbon_get_post_meta($product_id, 'certification'); ?></li>
                                 <li><i class="fas fa-circle-play"></i> Modalidad: <?php echo carbon_get_post_meta($product_id, 'mode'); ?></li>
                             </ul>
@@ -84,7 +84,7 @@ do_action( 'woocommerce_before_single_product' ); ?>
                 ?>
             </div>
 
-            <?php
+            <?php echo 'ID Producto', $product_id;
             // Obtener y mostrar content_blocks de Carbon Fields
             $content_blocks = carbon_get_post_meta($product_id, 'content_blocks');
 
