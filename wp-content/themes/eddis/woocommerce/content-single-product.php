d73213450acda2b604b1cf9e18cc5a6332afb263
Kuntur Studio <kunturstudio@gmail.com>
2025-07-02 16:38:39 -0300

    debugging active

--- wp-content/themes/eddis/woocommerce/content-single-product.php
+++ wp-content/themes/eddis/woocommerce/content-single-product.php
@@ -37,7 +37,9 @@ if ( $form_id ) {
     $is_form_active = carbon_get_post_meta( $form_id, 'eddis_form_active' ) === 1;
     $form_code = carbon_get_post_meta( $form_id, 'eddis_form_code' );
 }
-echo 'Form active: ', $is_form_active;
+
+echo 'Form ID: ', $form_id;
+echo 'Is active: ', $is_form_active;
 //echo 'Form active value: ', gettpe($is_form_active_value);
 wp_reset_postdata();
 // -------------------------------------------------------------
