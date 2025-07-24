d823eddd8a4102ac736dac51736df2eee7aa821c
Kuntur Studio <kunturstudio@gmail.com>
2025-07-23 21:23:25 -0300

    Banner de producto responsivo

--- wp-content/themes/eddis/custom-functions/custom-fields.php
+++ wp-content/themes/eddis/custom-functions/custom-fields.php
@@ -389,13 +389,13 @@ function edd_register_theme_options() {
 
             Field::make('image', 'product_banner_lg', 'Banner LG (≥1200px)')
                 ->set_value_type('url')
-                ->help_text('Tamaño recomendado: 1200px de ancho'),
+                ->help_text('Tamaño recomendado: 1920px de ancho'),
             Field::make('image', 'product_banner_md', 'Banner MD (≥768px)')
                 ->set_value_type('url')
-                ->help_text('Tamaño recomendado: 768px de ancho'),
+                ->help_text('Tamaño recomendado: 1200px de ancho'),
             Field::make('image', 'product_banner_sm', 'Banner SM (<768px)')
                 ->set_value_type('url')
-                ->help_text('Tamaño recomendado: 576px de ancho'),
+                ->help_text('Tamaño recomendado: 768px de ancho'),
 
             Field::make('checkbox', 'product_banner_enable_link', 'Habilitar enlace')
                 ->set_option_value('yes')
