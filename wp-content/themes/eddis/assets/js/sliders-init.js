df8231414fc3571ca199769ec830a8e298a2d658
Kuntur Studio <kunturstudio@gmail.com>
2025-08-18 17:07:24 -0300

    Features slider

--- wp-content/themes/eddis/assets/js/sliders-init.js
+++ wp-content/themes/eddis/assets/js/sliders-init.js
@@ -14,9 +14,20 @@ jQuery(function($) {
     initOwl('#sliderHero', {
         ...baseOptions,
         responsive: {
-            0: { items: 1, nav: true },
-            768: { items: 1, nav: false },
-            1200: { items: 1, nav: true, loop: false }
+            0: {items: 1, nav: false, dots: true},
+            768: {items: 1, nav: false},
+            1200: {items: 1, nav: true, loop: false}
+        }
+    });
+
+    initOwl('#featuredCarousel', {
+        ...baseOptions,
+        margin:20,
+        mouseDrag: true,
+        responsive:{
+            0: {items:1, nav:false},
+            768: {items:2, nav:false},
+            1200: {items:3, nav:true, loop:false}
         }
     });
 });
