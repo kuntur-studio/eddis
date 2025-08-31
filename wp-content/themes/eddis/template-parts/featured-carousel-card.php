78c1d2a1fd48051ad546d6fb84c14501d42286a0
Kuntur Studio <kunturstudio@gmail.com>
2025-08-31 18:49:39 -0300

    Eliminación de categorías y modificación de la imagen en el carrusel de cursos destacados.

--- wp-content/themes/eddis/template-parts/featured-carousel-card.php
+++ wp-content/themes/eddis/template-parts/featured-carousel-card.php
@@ -3,28 +3,35 @@
  * Card de producto para el carrusel de "Cursos Destacados"
  */
 
-$product       = wc_get_product(get_the_ID());
-$product_name  = $product->get_name();
-$product_price = $product->get_price_html();
-$product_image = get_the_post_thumbnail_url(get_the_ID(), 'medium');
-$product_link  = get_permalink();
+$product = wc_get_product(get_the_ID());
 
-$categories = wp_get_post_terms(get_the_ID(), 'product_cat', array('exclude' => array(2)));
+// Asegúrate de que el producto existe antes de obtener sus datos
+if ($product) {
+    $product_name = $product->get_name();
+    $product_price = $product->get_price_html();
+    $product_alt = get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true);
+    $product_link = get_permalink();
+}
 ?>
+
 <div class="item">
     <div class="global-card">
         <a class="global-card-anchor" href="<?php echo esc_url($product_link); ?>"></a>
-        <?php if ($product_image): ?>
-            <div class="imagen-de-fondo" style="background-image: url('<?php echo esc_url($product_image); ?>')"></div>
+
+        <?php if ($product && has_post_thumbnail()): ?>
+            <div class="imagen-container">
+                <?php
+                    // Muestra la imagen destacada del producto con los atributos correctos
+                    echo get_the_post_thumbnail(get_the_ID(), 'woocommerce_thumbnail', array(
+                        'class' => 'attachment-woocommerce_thumbnail size-woocommerce_thumbnail',
+                        'decoding' => 'async',
+                        'alt' => esc_attr($product_alt ? $product_alt : $product_name)
+                    ));
+                ?>
+            </div>
         <?php endif; ?>
 
-        <div class="global-card-body igualar">
-            <?php foreach ($categories as $category): ?>
-                <a href="<?php echo esc_url(get_term_link($category)); ?>">
-                    <?php echo esc_html($category->name); ?>
-                </a>
-            <?php endforeach; ?>
-
+        <div class="global-card-body">
             <h3><?php echo esc_html($product_name); ?></h3>
             <h6><?php echo wp_kses_post($product_price); ?></h6>
         </div>
