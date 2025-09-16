a4a3cc6e4ac4120fb71ed3cafaed0a88276d6b03
Kuntur Studio <kunturstudio@gmail.com>
2025-09-16 18:34:16 -0300

    Título y sub para widget de sedes en admin

--- wp-content/themes/eddis/custom-functions/custom-fields.php
+++ wp-content/themes/eddis/custom-functions/custom-fields.php
@@ -460,6 +460,9 @@ function edd_register_theme_options() {
 					Field::make('checkbox', 'enable_branches_widget', 'Activo')
 						->set_option_value('yes')
 						->set_default_value(true),
+					Field::make('text', 'branches_widget_title', 'Título')
+						->set_required(true),
+					Field::make('text', 'branches_widget_subtitle', 'Subítulo'),
 					Field::make('multiselect', 'branches_widget_pages', 'Páginas donde cargar')
 						->set_options(['all' => 'Todas las páginas'] + edd_get_pages_list())
 						->set_default_value(['home', '764', '770']),
