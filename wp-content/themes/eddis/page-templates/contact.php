c55d6238fd72cc40a8cd7a2053255f5f8b1eae93
Kuntur Studio <kunturstudio@gmail.com>
2025-10-07 21:14:35 -0300

    Fix form contacto #2

--- wp-content/themes/eddis/page-templates/contact.php
+++ wp-content/themes/eddis/page-templates/contact.php
@@ -32,7 +32,7 @@ get_header();
         </div>
     </section>
 
-    <?php if (!empty($form['active']) && !empty($form_code)): ?>
+    <?php if (!empty($form['active']) && !empty($form['code'])): ?>
     <section class="contacto">
         <div class="container">
             <div class="row">
