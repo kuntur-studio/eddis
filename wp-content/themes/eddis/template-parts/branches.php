b1b622ec98dad7675b90340eb17042b964c713de
Kuntur Studio <kunturstudio@gmail.com>
2025-10-15 14:58:02 -0300

    Agrego título y subtítulo del widgewt de sedes

--- wp-content/themes/eddis/template-parts/branches.php
+++ wp-content/themes/eddis/template-parts/branches.php
@@ -10,4 +10,18 @@ $classes = $args['classes'] ?? [];
 
 // Convierte el array de clases en un string separado por espacios
 $class_string = implode(' ', $classes);
-?>    <div id="branches-widget-root" class="<?php echo esc_attr($class_string); ?>"></div>
+
+// Obtengo parámetros extra del widget
+$widget_options = edd_widget_options('branches_widget');
+?>
+    <section class="sedes">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="global-title text-center"><?php echo wp_kses_post($widget_options['branches_widget_title']); ?></h2>
+                    <p class="global-subtitle text-center"><?php echo wp_kses_post($widget_options['branches_widget_subtitle']); ?></p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div id="branches-widget-root" class="<?php echo esc_attr($class_string); ?>"></div>
