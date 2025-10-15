9a60971be2886d9ed5a45fa250a904f351540632
Kuntur Studio <kunturstudio@gmail.com>
2025-10-15 15:00:10 -0300

    Elimino título  de prueba en la página de sedes

--- wp-content/themes/eddis/page-templates/branches.php
+++ wp-content/themes/eddis/page-templates/branches.php
@@ -6,7 +6,6 @@ get_header();
 ?>
 
 <div class="container my-5">
-    <h1>Nuestras Sedes</h1>
     <?php get_template_part('template-parts/branches', null, [
             'classes' => ['bottom_spacer_120']
         ]); ?>
